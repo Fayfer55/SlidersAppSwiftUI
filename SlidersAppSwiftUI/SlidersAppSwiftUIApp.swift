183561489fdcd1e70ab77c2f915436b8b3115aa3
Fayfer Kirill <sayfer555@gmail.com>
2020-10-19 19:08:14 +0600

    Initial Commit

---
+++ SlidersAppSwiftUI/SlidersAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlidersAppSwiftUIApp.swift
+//  SlidersAppSwiftUI
+//
+//  Created by Кирилл Файфер on 19.10.2020.
+//
+
+import SwiftUI
+
+@main
+struct SlidersAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
